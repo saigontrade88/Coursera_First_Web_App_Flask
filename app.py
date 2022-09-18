c3ff54a02d0c70f8ebb44348c601dab79e0c11fb
Long Dang <longdangvn88@gmail.com>
2022-09-17 22:35:16 -0400

    Create SQLAlchemy

--- app.py
+++ app.py
@@ -2,9 +2,18 @@
 https://code.visualstudio.com/docs/python/tutorial-flask
 '''
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
 
 app = Flask(__name__) # print(__name__)
 app.config['SECRET_KEY'] = 'cpr4llp' #need to update it
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +  \
+                                        os.path.join(BASE_DIR, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from route import *
 
